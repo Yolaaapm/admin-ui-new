cb3388346749eb4c0ab19144ed36c11e1ea5477b
Yolaaapm <monikfiola@gmail.com>
2025-12-06 12:08:34 +0700

    atomic design page sign in dan sign up

---
+++ src/components/Fragments/FormSignUp.jsx
@@ -0,0 +1,134 @@
+import React from "react";
+import LabeledInput from "../Elements/LabeledInput";
+import Button from "../Elements/Button";
+
+function FormSignUp() {
+  return (
+    <>
+      {/* Form Sign Up start */}
+      <div className="mt-16">
+        <form action="">
+          {/* 1. Input Nama Lengkap (Tambahan untuk Sign Up) */}
+          <div className="mb-6">
+            <LabeledInput 
+              label="Nama Lengkap"
+              id="name"
+              type="text"
+              placeholder="Masukkan nama Anda"
+              name="name"
+            />
+          </div>
+
+          {/* 2. Input Email (Sama seperti Sign In) */}
+          <div className="mb-6">
+            <LabeledInput 
+              label="Email Address"
+              id="email"
+              type="email"
+              placeholder="hello@example.com"
+              name="email"
+            />
+          </div>
+
+          {/* 3. Input Password (Sama seperti Sign In) */}
+          <div className="mb-6">
+            <LabeledInput 
+              label="Password"
+              id="password"
+              type="password"
+              placeholder="••••••••••••••••"
+              name="password"
+            />
+          </div>
+          
+          {/* 4. Input Konfirmasi Password (Opsional tapi disarankan untuk Sign Up) */}
+          <div className="mb-6">
+            <LabeledInput 
+              label="Konfirmasi Password"
+              id="confirm_password"
+              type="password"
+              placeholder="Ulangi password Anda"
+              name="confirm_password"
+            />
+          </div>
+
+          {/* Tombol Submit */}
+          <Button>Daftar</Button> 
+        </form>
+      </div>
+      {/* Form Sign Up end */}
+      
+      {/* Teks "or sign up with" start (Sama seperti Sign In) */}
+      <div className="my-9 px-7 flex flex-col justify-center items-center text-xs text-gray-03">
+        <div className="border border-gray-05 w-full"></div>
+        <div class="px-2 bg-special-mainBg absolute"> or sign up with</div>
+      </div>
+      {/* Teks "or sign up with" end */}
+      
+      {/* Sign Up with Google start (Sama seperti Sign In, tapi teks disesuaikan) */}
+      <div className="mb-8">
+        <Button type="button" variant="secondary">
+          <span className="flex items-center justify-center">
+            {/* SVG Google di sini */}
+            <svg
+            class="h-6 w-6 mr-2"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            width="800px"
+            height="800px"
+            viewBox="-0.5 0 48 48"
+            version="1.1"
+            >
+                {/* Isi path SVG Google... */}
+            <title>Google-color</title> <desc>Created with Sketch.</desc>
+            <defs> </defs>
+            <g
+                id="Icons"
+                stroke="none"
+                stroke-width="1"
+                fill="none"
+                fill-rule="evenodd"
+            >
+                <g id="Color-" transform="translate(-401.000000, -860.000000)">
+                <g id="Google" transform="translate(401.000000, 860.000000)">
+                    <path
+                    d="M9.82727273,24 C9.82727273,22.4757333 10.0804318,21.0144 10.5322727,19.6437333 L2.62345455,13.6042667 C1.08206818,16.7338667 0.213636364,20.2602667 0.213636364,24 C0.213636364,27.7365333 1.081,31.2608 2.62025,34.3882667 L10.5247955,28.3370667 C10.0772273,26.9728 9.82727273,25.5168 9.82727273,24"
+                    id="Fill-1"
+                    fill="#FBBC05"
+                    ></path>
+                    <path
+                    d="M23.7136364,10.1333333 C27.025,10.1333333 30.0159091,11.3066667 32.3659091,13.2266667 L39.2022727,6.4 C35.0363636,2.77333333 29.6954545,0.533333333 23.7136364,0.533333333 C14.4268636,0.533333333 6.44540909,5.84426667 2.62345455,13.6042667 L10.5322727,19.6437333 C12.3545909,14.112 17.5491591,10.1333333 23.7136364,10.1333333"
+                    id="Fill-2"
+                    fill="#EB4335"
+                    ></path>
+                    <path
+                    d="M23.7136364,37.8666667 C17.5491591,37.8666667 12.3545909,33.888 10.5322727,28.3562667 L2.62345455,34.3946667 C6.44540909,42.1557333 14.4268636,47.4666667 23.7136364,47.4666667 C29.4455,47.4666667 34.9177955,45.4314667 39.0249545,41.6181333 L31.5177727,35.8144 C29.3995682,37.1488 26.7323182,37.8666667 23.7136364,37.8666667"
+                    id="Fill-3"
+                    fill="#34A853"
+                    ></path>
+                    <path
+                    d="M46.1454545,24 C46.1454545,22.6133333 45.9318182,21.12 45.6113636,19.7333333 L23.7136364,19.7333333 L23.7136364,28.8 L36.3181818,28.8 C35.6879545,31.8912 33.9724545,34.2677333 31.5177727,35.8144 L39.0249545,41.6181333 C43.3393409,37.6138667 46.1454545,31.6490667 46.1454545,24"
+                    id="Fill-4"
+                    fill="#4285F4"
+                    ></path>
+                </g>
+                </g>
+            </g>
+            </svg>
+            Continue with Google
+          </span>
+        </Button>
+      </div>
+      {/* Sign Up with Google end */}
+      
+      {/* Link ke Sign In start */}
+      <div className="flex justify-center">
+        {/* Link ini mengarahkan ke halaman Login/Sign In */}
+        <a className="text-primary text-sm font-bold">I already have an account</a> 
+      </div>
+      {/* Link ke Sign In end */}
+    </>
+  );
+}
+
+export default FormSignUp;
