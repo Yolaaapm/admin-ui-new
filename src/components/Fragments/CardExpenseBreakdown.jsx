614572b0348fb67d1c232a3cd0665377102ae7ec
Yolaaapm <monikfiola@gmail.com>
2025-12-26 23:42:24 +0700

    12. State in Component with Hooks tapi pada CardUpcomingBill ukuran logonya belum sesuai

--- src/components/Fragments/CardExpenseBreakdown.jsx
+++ src/components/Fragments/CardExpenseBreakdown.jsx
@@ -6,38 +6,37 @@ function CardExpenseBreakdown(props) {
   const { data } = props;
 
   return (
-    <>
-      <Card
-        title="Expenses Breakdown"
-        				desc={
-          <div className="h-full md:grid md:grid-cols-3 gap-4">
-            {data.map((item) => (
-              <div key={item.id} className="flex items-center justify-between">
-                <div className="flex">
-                  <div>
-                    <div className="bg-special-bg text-gray-02 px-3 py-5 rounded-lg flex flex-col place-content-center">
-                      {item.icon}
-                    </div>
+    <Card
+      title="Expenses Breakdown"
+      desc={
+        <div className="h-full md:grid md:grid-cols-3 gap-4">
+          {data.map((item) => (
+            <div key={item.id} className="flex items-center justify-between">
+              <div className="flex">
+                <div>
+                  <div className="bg-special-bg px-3 py-5 rounded-lg flex flex-col place-content-center">
+                    {React.cloneElement(item.icon, { color: "#000000" })}
                   </div>
-                  <div className="ms-4">
+                </div>
+                <div className="ms-4">
                     <span className="text-gray-02">{item.category}</span>
                     <br />
                     <span className="font-bold text-lg">${item.amount}</span>
-                    <div className="flex">
+                  <div className="flex">
                       <span className="text-gray-02">{item.percentage}%*</span>{" "}
                       {item.arrow}
-                    </div>
                   </div>
                 </div>
-                <div className="flex place-content-center flex-col me-8">
-                  <Icon.ArrowRight />
-                </div>
               </div>
-            ))}
-          </div>
-        }
-      />
-    </>
+
+              <div className="flex place-content-center flex-col me-8">
+                  <Icon.ArrowRight />
+              </div>
+            </div>
+          ))}
+        </div>
+      }
+    />
   );
 }
 
